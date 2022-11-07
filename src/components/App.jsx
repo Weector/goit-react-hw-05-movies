3163f0a7b22e1854f2d8b6842b602334ddaa4ecc
Victor Grossu <weekgrossu@gmail.com>
2022-11-07 15:26:27 +0200

    Create start project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home/Home';
+import Movies from './Movies/Movies';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 };
